9af8e1569537353f68e3df03f85771e13c41a3f8
Mohammad Aakil <aakilk735@gmail.com>
2025-02-14 19:49:28 +0530

    Add files via upload

---
+++ VowelConsonante.java
@@ -0,0 +1,20 @@
+public class VowelConsonante {
+        public static void main(String[] args) {
+
+            String ch = "l";
+
+            if (ch == "a" || ch == "e" || ch == "i" || ch == "o" || ch == "u"
+            || ch == "A"|| ch == "E" || ch == "I" || ch == "O" || ch == "U") {
+
+                System.out.println(ch +" is a vowel");
+
+            } else {
+
+                System.out.println(ch + " is a consonent");
+
+            }
+
+        }
+
+    }
+
